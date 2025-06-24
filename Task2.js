878f02bf8d8b697cd759c11f35221b8a2a2e0442
nahmanbo <nahmanbo2003@gmail.com>
2025-06-24 17:04:33 +0300

    Update Task2.js

--- Task2.js
+++ Task2.js
@@ -1,6 +1,11 @@
+function getEvenNumbers(numbers) {
+    const isEven = (num) => {
+        return num % 2 === 0;
+    };
+
+    const evenNumbers = numbers.filter(isEven);
+    return evenNumbers;
+}
+
 const numbers = [1, 2, 3, 4, 5];
-
-const isEven = (num) => {return num % 2 === 0;}
-
-const evenNumbers = numbers.filter(isEven);
-console.log(evenNumbers);
+console.log(getEvenNumbers(numbers)); 
