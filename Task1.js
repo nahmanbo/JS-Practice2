3feb28a1935548ac839bf3853f40e103da7345ff
nahmanbo <nahmanbo2003@gmail.com>
2025-06-24 17:03:42 +0300

    Update Task1.js

--- Task1.js
+++ Task1.js
@@ -1,6 +1,11 @@
+function doubleNumbersArray(numbers) {
+    const multiply2 = (num) => {
+        return num * 2;
+    };
+
+    const doubleNumbers = numbers.map(multiply2);
+    return doubleNumbers;
+}
+
 const numbers = [1, 2, 3, 4, 5];
-
-const multiply2 = (num) => {return num * 2;}
-
-const doubleNumbers = numbers.map(multiply2);
-console.log(doubleNumbers);
+console.log(doubleNumbersArray(numbers)); 
